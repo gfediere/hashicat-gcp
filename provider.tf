ea4c2b84303414e9b660239f2f9ddbb8888b02ee
Guillaume Fediere <gfediere@gmail.com>
2023-04-17 11:34:01 +0200

    Initial commit

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+  cloud {
+    organization = "guillaume-org"
+    workspaces {
+      name = "easyDeploy-GCP"
+    }
+  }
+}
